944d7ca3f24cacc26159780ad8b5727ff06b976f
mollonr <mollonr@gmail.com>
2017-03-26 23:14:06 -0300

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*
+cpphelloworld.cpp
+TP#0 "Hello World!" - AED_2017_K1051
+Rodrigo Mollon, Leg. 162821
+V_20170326.0
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!";
+	return 0;
+}
